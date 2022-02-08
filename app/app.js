224cefdcc8e46d4d3867ed4a3e40eba7d787d457
Fredson Rodrigues <fredson.rodrigues.principal@gmail.com>
2022-02-08 15:20:49 -0300

    feat: add autocomplete for hero search

--- app/app.js
+++ app/app.js
@@ -1,8 +1,16 @@
 'use strict';
 
+if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function (searchString, position) {
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+  };
+}
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
   'ngRoute',
+  'ngSanitize',
+  'autoCompleteModule',
   'myApp.heroSearch',
   'myApp.version'
 ]).
